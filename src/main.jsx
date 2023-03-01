ec4fcec6be984e149f8547a874b23feaf93f6894
Diego Vistoso <diegoivg98@gmail.com>
2023-03-01 18:14:21 -0300

    agregar Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { createAppStore } from "./store/config/storeConfig";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+let appStore = createAppStore()
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={appStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);

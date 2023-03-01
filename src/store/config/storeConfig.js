9449cd5ffee9619e3e28baed92c5594231140c9b
Diego Vistoso <diegoivg98@gmail.com>
2023-03-01 18:15:33 -0300

    agregar actions, config y reducers al store

---
+++ src/store/config/storeConfig.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { rootReducer } from "../reducers/rootReducer";
+
+export const createAppStore = () => {
+    let store = configureStore({
+      reducer: rootReducer,
+    }, composeWithDevTools());
+  
+    return store;
+  };

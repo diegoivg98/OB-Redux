37c1db39f676e3b53b24bf69ee4d72d0b16f1618
Diego Vistoso <diegoivg98@gmail.com>
2023-03-02 19:24:36 -0300

    agregar componentes a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import TodoContainer from './components/containers/TodoContainer'
+import TodoFormContainer from './components/containers/TodoFormContainer'
+import FilterOptions from './components/pure/FilterOptions'
 
 function App() {
 
   return (
     <div className="App">
-      
+    <TodoContainer></TodoContainer>
+    <TodoFormContainer></TodoFormContainer>
+    <FilterOptions></FilterOptions>
     </div>
   )
 }

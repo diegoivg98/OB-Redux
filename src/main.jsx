b63f5834f53b013fd720edfedd51b4765b7277cb
Diego Vistoso <diegoivg98@gmail.com>
2023-03-06 16:52:02 -0300

    agregar llamada a AppReduxSaga en el main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
+// import App from "./App";
+import AppReduxSaga from "./AppReduxSaga";
 import "./index.css";
 import { createAppStore } from "./store/config/storeConfig";
 
@@ -10,7 +11,8 @@ let appStore = createAppStore()
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={appStore}>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <AppReduxSaga/>
     </React.StrictMode>
   </Provider>
 );

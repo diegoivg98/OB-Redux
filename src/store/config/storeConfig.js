0c79b46f6fc301d8c15ee06ac83275782e8244c7
Diego Vistoso <diegoivg98@gmail.com>
2023-03-06 21:35:52 -0300

    agregar sagaMiddleware y compose para createAppAsyncStore

--- src/store/config/storeConfig.js
+++ src/store/config/storeConfig.js
@@ -1,11 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit"
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { rootReducer } from "../reducers/rootReducer";
+import createSagaMiddleware from "@redux-saga/core";
+import { configureStore, compose} from "@reduxjs/toolkit";
+import { workerSaga } from "../sagas/sagas";
 
 export const createAppStore = () => {
-    let store = configureStore({
+  let store = configureStore(
+    {
       reducer: rootReducer,
-    }, composeWithDevTools());
-  
-    return store;
-  };
+    },
+    composeWithDevTools()
+  );
+
+  return store;
+};
+
+export const createAppAsyncStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  let store = configureStore(
+    {
+      reducer: rootReducer,
+    },
+    compose(sagaMiddleware, composeWithDevTools())
+  );
+
+  sagaMiddleware.run(workerSaga);
+
+  return store;
+};

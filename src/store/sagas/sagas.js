8cc28ec6f0f210ef8e565ec30ac5b1a60b3be966
Diego Vistoso <diegoivg98@gmail.com>
2023-03-07 19:38:24 -0300

    cambios en las sagas

--- src/store/sagas/sagas.js
+++ src/store/sagas/sagas.js
@@ -1,33 +1,39 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
-import axios from 'axios'
-import { API_CALL_REQUEST } from '../actions/asyncActions'
+import { takeLatest, call, put } from 'redux-saga/effects';
+import axios from 'axios';
+import { API_CALL_REQUEST } from '../actions/asyncActions';
 
-export function* watcherSaga() {
+// Watcher SAGA
+// Listens the API_CALL_REQUEST actions
+export function* watcherSaga(){
+    // Listens the action and starts a Worker Saga
     yield takeLatest(API_CALL_REQUEST, workerSaga)
 }
 
-export function* workerSaga() {
+// WORKER SAGA
+// Is called from watcher Saga, does the Login and Dispaches an action
+export function* workerSaga(action){
     try {
         const response = yield call(fetchHttp(action.payload.request))
-        const token = response.data.token
+        // We Obtain the token from response
+        const token = response.data.token;
         yield put({
-            type: action.payload.okAction,
+            type: action.payload.okAction, // API_CALL_SUCCESS
             payload: {
                 token: token
             }
-        })
+        });
     } catch (error) {
         yield put({
-            type: action.payload.failAction,
+            type: action.payload.failAction, // API_CALL_FAILURE
             payload: {
                 error: error
             }
-        })
+        });
     }
 }
 
-function fetchHttp(request) {
-    return function () {
-        return (axios(request))
+function fetchHttp(request){
+    return function(){
+        return(axios(request))
     }
 }

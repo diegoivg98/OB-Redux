f1c84ec41753a2612766b3edace4baafe21cf0ff
Diego Vistoso <diegoivg98@gmail.com>
2023-03-07 19:43:05 -0300

    serie de funciones que devuelve un tipo de acción

--- src/store/actions/asyncActions.js
+++ src/store/actions/asyncActions.js
@@ -1,20 +1,22 @@
-export const API_CALL_REQUEST = "API_CALL_REQUEST";
-export const API_CALL_SUCCESS = "API_CALL_SUCCESS";
-export const API_CALL_FAILURE = "API_CALL_FAILURE";
+export const API_CALL_REQUEST = 'API_CALL_REQUEST';
+export const API_CALL_SUCCESS = 'API_CALL_SUCCESS';
+export const API_CALL_FAILURE = 'API_CALL_FAILURE';
 
 export const login = (email, password) => {
   return {
     type: API_CALL_REQUEST,
     payload: {
-      method: "post",
-      url: "https://reqres.in/api/login",
-      data: {
-        email: email,
-        password: password,
+      request: {
+        method: 'post',
+        url: 'https://reqres.in/api/login',
+        data: {
+          email: email,
+          password: password,
+        }
       },
       okAction: API_CALL_SUCCESS,
-      failAction: API_CALL_FAILURE,
-    },
+      failAction: API_CALL_FAILURE
+    }
   };
 };
 
@@ -22,11 +24,13 @@ export const httpRequest = (method, url, data) => {
   return {
     type: API_CALL_REQUEST,
     payload: {
-      method: method,
-      url: url,
-      data: data,
+      request: {
+        method: method,
+        url: url,
+        data: data,
+      },
       okAction: API_CALL_SUCCESS,
-      failAction: API_CALL_FAILURE,
-    },
+      failAction: API_CALL_FAILURE
+    }
   };
 };

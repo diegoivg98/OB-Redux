9c8ba88815d432bbf4f8f2e9e68a1ffc3464ef6e
Diego Vistoso <diegoivg98@gmail.com>
2023-03-07 19:39:42 -0300

    cambios en el store de createAppAsyncStore

--- src/store/config/storeConfig.js
+++ src/store/config/storeConfig.js
@@ -1,8 +1,8 @@
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { rootReducer } from "../reducers/rootReducer";
+import { configureStore} from '@reduxjs/toolkit';
+import { watcherSaga } from "../sagas/sagas";
 import createSagaMiddleware from "@redux-saga/core";
-import { configureStore, compose} from "@reduxjs/toolkit";
-import { workerSaga } from "../sagas/sagas";
 
 export const createAppStore = () => {
   let store = configureStore(
@@ -17,15 +17,16 @@ export const createAppStore = () => {
 
 export const createAppAsyncStore = () => {
   const sagaMiddleware = createSagaMiddleware();
-
   let store = configureStore(
     {
       reducer: rootReducer,
-    },
-    compose(sagaMiddleware, composeWithDevTools())
+      middleware: [sagaMiddleware],
+      devTools: composeWithDevTools(),
+    }
   );
 
-  sagaMiddleware.run(workerSaga);
+  // We init the Watcher Saga
+  sagaMiddleware.run(watcherSaga);
 
   return store;
 };

01390483c78c34ddc41791574ee7e2f5f6d48da4
Diego Vistoso <diegoivg98@gmail.com>
2023-03-07 19:44:09 -0300

    agregar TodoContainer, FormContainer y FilterOptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import TodoContainer from './components/containers/TodoContainer'
-import TodoFormContainer from './components/containers/TodoFormContainer'
-import FilterOptions from './components/pure/FilterOptions'
+import "./App.css";
+import TodoContainer from "./components/containers/TodoContainer";
+import TodoFormContainer from "./components/containers/TodoFormContainer";
+import FilterOptions from "./components/pure/FilterOptions";
 
 function App() {
-
   return (
-    <div className="App">
-    <TodoContainer></TodoContainer>
-    <TodoFormContainer></TodoFormContainer>
-    <FilterOptions></FilterOptions>
-    </div>
-  )
+      <div className="App">
+        <TodoContainer />
+        <TodoFormContainer />
+        <FilterOptions />
+      </div>
+  );
 }
 
-export default App
+export default App;
